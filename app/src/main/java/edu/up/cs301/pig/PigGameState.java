18632b399043e5eb5dfc998f009a26e0eb037cf6
William <cloutier23@up.edu>
2020-10-06 15:12:35 -0700

    CHeckpoint1

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -4,4 +4,61 @@ import edu.up.cs301.game.infoMsg.GameState;
 
 public class PigGameState extends GameState {
     private int ID = 0;
+    private int p1Score = 0;
+    private int p2Score = 0;
+    private int runningTotal = 0;
+    private int dieValue = 0;
+
+
+    public PigGameState(){
+        this.ID = 0;
+        this.p1Score = 0;
+        this.p2Score = 0;
+        this.runningTotal = 0;
+        this.dieValue = 0;
+    }
+    public PigGameState(PigGameState name){
+        PigGameState copy = name;
+        copy.ID = name.ID;
+        copy.p1Score = name.p1Score;
+        copy.p2Score = name.p2Score;
+        copy.runningTotal = name.runningTotal;
+        copy.dieValue = name.dieValue;
+
+    }
+
+    //getters
+    public int getID(){
+        return this.ID;
+    }
+    public int getP1Score(){
+        return this.p1Score;
+    }
+    public int getP2Score(){
+        return this.p2Score;
+    }
+    public int getRunningTotal(){
+        return this.runningTotal;
+    }
+    public int getDieValue(){
+        return this.dieValue;
+    }
+
+    //setters
+    public void setID(int set){
+        this.ID = set;
+    }
+    public void setP1Score(int set){
+        this.p1Score = set;
+    }
+    public void setP2Score(int set){
+       this.p2Score = set;
+    }
+    public void setRunningTotal(int set){
+        this.runningTotal = set;
+    }
+    public void setDieValue(int set){
+         this.dieValue = set;
+    }
+
 }
