8777ba70d6d51279f3a0db29cfcce682580e230c
William <cloutier23@up.edu>
2020-10-12 15:10:19 -0700

    Checkpoint5 that makes smart ai player

---
+++ app/src/main/java/edu/up/cs301/pig/PigSmartComputerPlayer.java
@@ -0,0 +1,84 @@
+package edu.up.cs301.pig;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.game.infoMsg.GameState;
+import edu.up.cs301.game.util.Tickable;
+
+/**
+ * An AI for Pig
+ *
+ * @author Andrew M. Nuxoll
+ * @version August 2015
+ */
+public class PigSmartComputerPlayer extends GameComputerPlayer {
+    Random rand = new Random();
+
+    /**
+     * ctor does nothing extra
+     */
+    public PigSmartComputerPlayer(String name) {
+        super(name);
+
+    }
+
+    /**
+     * callback method--game's state has changed
+     *
+     * @param info
+     * 		the information (presumably containing the game's state)
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+        if(!(info instanceof PigGameState)) {
+            return;
+        }
+
+
+        PigGameState copy = (PigGameState) info;
+        if(copy.getID() == playerNum){
+
+
+            if(50-copy.getP1Score() > 40 || 50-copy.getP2Score() > 40){
+                    if(copy.getRunningTotal() > 3){
+                        PigHoldAction hold = new PigHoldAction(this);
+                        game.sendAction(hold);
+                    }
+                    else{
+                        PigRollAction roll = new PigRollAction(this);
+                        game.sendAction(roll);
+                    }
+                }
+                else if(Math.abs(copy.getP1Score() - copy.getP2Score()) > 10){
+                    if(copy.getRunningTotal() > 10){
+                        PigHoldAction hold = new PigHoldAction(this);
+                        game.sendAction(hold);
+                    }
+                    else{
+                        PigRollAction roll = new PigRollAction(this);
+                        game.sendAction(roll);
+                    }
+                }
+                else{
+                    if(copy.getRunningTotal() > 5){
+                        PigHoldAction hold = new PigHoldAction(this);
+                        game.sendAction(hold);
+                    }
+                    else{
+                        PigRollAction roll = new PigRollAction(this);
+                        game.sendAction(roll);
+                    }
+                }
+
+            }
+        else{
+            return;
+        }
+    }//receiveInfo
+
+}
